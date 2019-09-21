c05b18ceae54b7d6620ea2bb5e02b54b1a6fb8ad
Casper Boone <mail@casperboone.nl>
2019-09-21 15:03:24 +0200

    Return filtered directory

--- project_filter.py
+++ project_filter.py
@@ -2,6 +2,11 @@ import os
 
 
 class ProjectFilter:
-    def filter_directory(self, directory: str, extension: str = '.py') -> None:
+    def filter_directory(self, directory: str, extension: str = '.py') -> str:
+        """
+        Delete all files within the given directory with filenames not ending in the given extension
+        """
         for root, dirs, files in os.walk(directory):
             [os.remove(os.path.join(root, fi)) for fi in files if not fi.endswith(extension)]
+
+        return directory

36d279590dd8da91cd68ec1e7c38dafb0bd01796
Niels <nielsdebruin@gmail.com>
2019-09-29 15:41:08 +0200

    Clean

--- utils.py
+++ utils.py
@@ -30,12 +30,3 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
             return Parallel(**joblib_args)(bar_func(op_iter))
         return tmp
     return aprun
-
-
-
-# aprun = ParallelExecutor(n_jobs=5)
-#
-# a1 = aprun(total=25)(delayed(func)(i ** 2 + j) for i in range(5) for j in range(5))
-# a2 = aprun(total=16)(delayed(func)(i ** 2 + j) for i in range(4) for j in range(4))
-# a2 = aprun(bar='txt')(delayed(func)(i ** 2 + j) for i in range(4) for j in range(4))
-# a2 = aprun(bar=None)(delayed(func)(i ** 2 + j) for i in range(4) for j in range(4))

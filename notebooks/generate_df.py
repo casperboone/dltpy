e2939551b39130419d841edbfaf735b5c08fc856
Niels <nielsdebruin@gmail.com>
2019-10-05 19:52:48 +0200

    Add setup to remove type from dataset

--- notebooks/generate_df.py
+++ notebooks/generate_df.py
@@ -5,6 +5,8 @@ import numpy as np
 # CONFIG
 
 # project root
+from sklearn import preprocessing
+
 PR = "../"
 DATA_FILES_DIR = os.path.join(PR, "output/data")
 
@@ -20,46 +22,124 @@ def list_data_files(datadir, full=True):
     return paths
 
 
-def parse_df(files, batch_size=10):
-    df = None
+def parse_df(files, batch_size=10) -> pd.DataFrame:
+    df_merged = None
 
     batch = []
+    total = 0
     for i, file in enumerate(files):
         if i % 100 == 0:
             print(f"Loaded {i}/{len(files)} files")
 
-        if df is None:
-            df = pd.read_csv(file)
+        _loaded = pd.read_csv(file, index_col=0)
+        total += len(_loaded)
+        if df_merged is None:
+            df_merged = _loaded
         else:
-            batch.append(pd.read_csv(file))
+            batch.append(_loaded)
 
         if i % batch_size == 0 and i > 0:
-            df = pd.concat([df, *batch])
+            frames = [df_merged, *batch]
+            df_merged = pd.concat(frames, ignore_index=True)
             batch = []
 
     if len(batch) > 0:
-        df = pd.concat([df, *batch])
+        df_merged = pd.concat([df_merged, *batch])
+
+    df_merged = df_merged.reset_index(drop=True)
+    assert total == len(df_merged), "Length of all loaded should be equal to merged"
+
+    return df_merged
+
+
+def encode_types(df):
+    le = preprocessing.LabelEncoder()
+
+    # All types
+    return_types = df['return_type'].values
+    arg_types = np.hstack(df['arg_types'].values)
+    rt = np.concatenate((return_types, arg_types), axis=0)
+    unique, counts = np.unique(rt, return_counts=True)
+    print(f"Found {len(unique)} unique types in a total of {len(rt)} types.")
+
+    # type we are going to keep
+    print("Remapping uncommon types for functions")
+    common_types = [unique[i] for i in np.argsort(counts)[::-1][:999]]
+    df['return_type_t'] = df['return_type'].apply(lambda x: x if x in common_types else 'other')
+    print("Remapping uncommon types for arguments")
+    df['arg_types_t'] = df['arg_types'].apply(lambda x: [i if i in common_types else 'other' for i in x])
+
+    print("Fitting label encoder")
+    # All types transformed
+    return_types = df['return_type_t'].values
+    arg_types = np.hstack(df['arg_types_t'].values)
+    rt = np.concatenate((return_types, arg_types), axis=0)
+    le.fit(rt)
+
+    #     print(le.classes_)
+
+    # transform all type
+    print("Transforming return types")
+    df['return_type_enc'] = le.transform(return_types)
+
+    print("Transforming args types")
+    df['arg_types_enc'] = df['arg_types_t'].apply(lambda x: le.transform(x))
 
     return df
 
 
 def format_df(df):
-    """Add """
-    df['arg_names'] = df['arg_names'].apply(lambda x: tuple(eval(x)))
-    df['arg_types'] = df['arg_types'].apply(lambda x: np.asarray(eval(x)))
+    """Format all array type to real arrays"""
+    df['arg_names'] = df['arg_names'].apply(lambda x: eval(x))
+    df['arg_types'] = df['arg_types'].apply(lambda x: eval(x))
+    df['arg_descrs'] = df['arg_descrs'].apply(lambda x: eval(x))
+    df['return_expr'] = df['return_expr'].apply(lambda x: eval(x))
 
     return df
 
-
 if __name__ == '__main__':
-    DATA_FILES = list_data_files(DATA_FILES_DIR)
-    print("Found %d datafiles" % len(DATA_FILES))
+    DATA_FILE = "_temp_2.csv"
+    CACHE = True
 
-    df = parse_df(DATA_FILES, batch_size=32)
-    print("Dataframe loaded!")
+    if CACHE:
+        # Full DataFrame
+        df = pd.read_csv(DATA_FILE)
+    else:
+        DATA_FILES = list_data_files(DATA_FILES_DIR)
+        print("Found %d datafiles" % len(DATA_FILES))
 
-    print("Formatting dataframe")
+        df = parse_df(DATA_FILES, batch_size=128)
+        print("Dataframe loaded!")
+
+        print("Formatting dataframe")
+        # df = format_df(df)
+        print(df.head())
+
+        print("Dataframe formatted, writing it...")
+        df.to_csv("_temp_2.csv", index=False)
+
+    print(f"Functions before dropping on return type {len(df)}")
+    df = df.dropna(subset=['return_type'])
+    print(f"Functions after dropping on return type {len(df)}")
+
+    print(f"Functions before dropping nan return type {len(df)}")
+    to_drop = np.invert((df['return_type'] == 'nan') | (df['return_type'] == 'None'))
+    df = df[to_drop]
+    print(f"Functions after dropping nan return type {len(df)}")
+
+    print(f"Functions before dropping on empty docstring {len(df)}")
+    df = df.dropna(subset=['docstring'])
+    print(f"Functions after dropping on empty docstring {len(df)}")
+
+    # Format dataframe
     df = format_df(df)
+    df = encode_types(df)
 
-    print("Dataframe formatted, writing it...")
-    df.to_csv("_temp.csv")
+    print(f"Functions before dropping on empty return expression {len(df)}")
+    df = df[df['return_expr'].apply(len) > 0]
+    print(f"Functions after dropping on empty return expression {len(df)}")
+
+    # Drop all columns useless for the ML algorithms
+    df = df.drop(columns=['file', 'author'])
+
+    df.to_csv("_ml_inputs.csv", index=False)

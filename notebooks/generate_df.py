3209eda714e95834e8aeb142f0424eb1f2621a7d
Niels <nielsdebruin@gmail.com>
2019-10-06 16:44:14 +0200

    Add function to encode type, generate parameter df and refactor

--- notebooks/generate_df.py
+++ notebooks/generate_df.py
@@ -11,20 +11,34 @@ PR = "../"
 DATA_FILES_DIR = os.path.join(PR, "output/data")
 
 
-def list_data_files(datadir, full=True):
+def list_files(directory: str, full=True) -> list:
+    """
+    List all files in the given directory.
+    
+    :param directory: directory to search
+    :param full: whether to return the full path
+    :return: list of paths
+    """
     paths = []
-    for file in os.listdir(datadir):
-        if not os.path.isfile(os.path.join(datadir, file)):
+    for file in os.listdir(directory):
+        if not os.path.isfile(os.path.join(directory, file)):
             continue
 
-        paths.append(os.path.join(datadir, file) if full else file)
+        paths.append(os.path.join(directory, file) if full else file)
 
     return paths
 
 
-def parse_df(files, batch_size=10) -> pd.DataFrame:
-    df_merged = None
+def parse_df(files: list, batch_size=10) -> pd.DataFrame:
+    """
+    Given a set of files, load each as a dataframe and concatenate the result
+    into a large file dataframe containing the data from all files.
 
+    :param files: the files to load
+    :param batch_size: batch size used for concatenation
+    :return: final dataframe
+    """
+    df_merged = None
     batch = []
     total = 0
     for i, file in enumerate(files):
@@ -52,7 +66,63 @@ def parse_df(files, batch_size=10) -> pd.DataFrame:
     return df_merged
 
 
-def encode_types(df):
+def format_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Formats the loaded dataframe.
+    :type df: dataframe to use
+    :returns: final dataframe
+    """
+    df['arg_names'] = df['arg_names'].apply(lambda x: eval(x))
+    df['arg_types'] = df['arg_types'].apply(lambda x: eval(x))
+    df['arg_descrs'] = df['arg_descrs'].apply(lambda x: eval(x))
+    df['return_expr'] = df['return_expr'].apply(lambda x: eval(x))
+
+    return df
+
+
+def filter_functions(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Filters functions which are note useful.
+    :param df: dataframe to use
+    :return: filtered dataframe
+    """
+    print(f"Functions before dropping on return type {len(df)}")
+    df = df.dropna(subset=['return_type'])
+    print(f"Functions after dropping on return type {len(df)}")
+
+    print(f"Functions before dropping nan return type {len(df)}")
+    to_drop = np.invert((df['return_type'] == 'nan') | (df['return_type'] == 'None'))
+    df = df[to_drop]
+    print(f"Functions after dropping nan return type {len(df)}")
+
+    print(f"Functions before dropping on empty docstring {len(df)}")
+    df = df.dropna(subset=['docstring'])
+    print(f"Functions after dropping on empty docstring {len(df)}")
+
+    return df
+
+
+def gen_argument_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generates a new dataframe containing all argument data.
+    :param df: dataframe for which to extract argument
+    :return: argument dataframe
+    """
+    arguments = []
+    for i, row in df.iterrows():
+        for p_i, arg_name in enumerate(row['arg_names']):
+            if arg_name != 'self':
+                arguments.append([row['name'], arg_name, row['arg_types'][p_i], row['arg_descrs'][p_i]])
+
+    return pd.DataFrame(arguments, columns=['func_name', 'arg_name', 'arg_type', 'arg_comment'])
+
+
+def encode_types(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Encode the dataframe types to integers.
+    :param df:
+    :return:
+    """
     le = preprocessing.LabelEncoder()
 
     # All types
@@ -88,24 +158,22 @@ def encode_types(df):
     return df
 
 
-def format_df(df):
-    """Format all array type to real arrays"""
-    df['arg_names'] = df['arg_names'].apply(lambda x: eval(x))
-    df['arg_types'] = df['arg_types'].apply(lambda x: eval(x))
-    df['arg_descrs'] = df['arg_descrs'].apply(lambda x: eval(x))
-    df['return_expr'] = df['return_expr'].apply(lambda x: eval(x))
-
-    return df
-
 if __name__ == '__main__':
     DATA_FILE = "_temp_2.csv"
+    FILTERED_DATA_FILE = "_temp_filtered.csv"
     CACHE = True
 
     if CACHE:
-        # Full DataFrame
-        df = pd.read_csv(DATA_FILE)
+        if os.path.exists(FILTERED_DATA_FILE):
+            print("Loading filtered cached copy")
+            df = pd.read_csv(FILTERED_DATA_FILE)
+        else:
+            print("Loading cached copy")
+            df = pd.read_csv(DATA_FILE)
+            df = filter_functions(df)
+            df.to_csv(FILTERED_DATA_FILE, index=False)
     else:
-        DATA_FILES = list_data_files(DATA_FILES_DIR)
+        DATA_FILES = list_files(DATA_FILES_DIR)
         print("Found %d datafiles" % len(DATA_FILES))
 
         df = parse_df(DATA_FILES, batch_size=128)
@@ -116,23 +184,18 @@ if __name__ == '__main__':
         print(df.head())
 
         print("Dataframe formatted, writing it...")
-        df.to_csv("_temp_2.csv", index=False)
-
-    print(f"Functions before dropping on return type {len(df)}")
-    df = df.dropna(subset=['return_type'])
-    print(f"Functions after dropping on return type {len(df)}")
-
-    print(f"Functions before dropping nan return type {len(df)}")
-    to_drop = np.invert((df['return_type'] == 'nan') | (df['return_type'] == 'None'))
-    df = df[to_drop]
-    print(f"Functions after dropping nan return type {len(df)}")
-
-    print(f"Functions before dropping on empty docstring {len(df)}")
-    df = df.dropna(subset=['docstring'])
-    print(f"Functions after dropping on empty docstring {len(df)}")
+        df.to_csv(DATA_FILE, index=False)
 
     # Format dataframe
+    print("Formatting dataframe")
     df = format_df(df)
+
+    # Split df
+    print("Extracting arguments")
+    df_params = gen_argument_df(df)
+    print(f"Extracted a total of {len(df_params)} arguments.")
+
+    # Encode types as int
     df = encode_types(df)
 
     print(f"Functions before dropping on empty return expression {len(df)}")

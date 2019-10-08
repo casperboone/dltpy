86ec987717d21f185ec7a03befa79974203b0618
alangerak <a.c.langerak@student.tudelft.nl>
2019-10-08 21:10:29 +0200

    using splitted dataframes

--- embedder.py
+++ embedder.py
@@ -1,35 +1,36 @@
-from gensim.models import Word2Vec, KeyedVectors
+from gensim.models import Word2Vec
 import pandas as pd
 import multiprocessing
 import itertools
-import numpy as np
+import os
 from typing import Iterator, List, Callable
 
-import time
-import logging
-logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S', level=logging.INFO)
-
+#CONFIG
+OUTPUT_DIRECTORY = os.path.join('./output')
+PARAM_DF_PATH = os.path.join(OUTPUT_DIRECTORY, 'ml_inputs', '_ml_param.csv')
+RETURN_DF_PATH = os.path.join(OUTPUT_DIRECTORY, 'ml_inputs', '_ml_return.csv')
+OUTPUT_EMBEDDINGS_DIRECTORY = os.path.join('./output', 'embeddings')
+LANGUAGE_EMBEDDING_OUTPUT_PATH = os.path.join(OUTPUT_EMBEDDINGS_DIRECTORY, 'w2v_language_model.bin')
+CODE_EMBEDDING_OUTPUT_PATH = os.path.join(OUTPUT_EMBEDDINGS_DIRECTORY, 'w2v_code_model.bin')
 
 
 class Embedder:
     """
-    Create a word embeddings using Word2Vec.
+    Create embeddings for the code names and docstring names using Word2Vec.
     """
 
-    def __init__(self, file: str) -> None:
-        self.df = pd.read_csv(file)
-
-    def __init__(self, df) -> None:
-        self.df = df
+    def __init__(self, param_df: pd.DataFrame, return_df: pd.DataFrame) -> None:
+        self.param_df = param_df
+        self.return_df = return_df
 
     def getLanguageIterator(self) -> Iterator[List[str]]:
         """
         Get an iterator over the whole collection of descriptions language.
         :return: iterator over the language data
         """
-        func_descr_sentences = (row.split() for row in self.df['func_descr'] if not isinstance(row, float))
-        arg_descr_sentences = (arg_descr.split() for arg_descrs_method in self.df['arg_descrs'] for arg_descr in eval(arg_descrs_method))
-        return_descr_sentences = (row.split() for row in self.df['return_descr'] if not isinstance(row, float))
+        func_descr_sentences = (row.split() for row in self.return_df['func_descr'])
+        arg_descr_sentences = (arg_descr.split() for arg_descr in param_df['arg_comment'])
+        return_descr_sentences = (row.split() for row in self.return_df['return_descr'])
         all_comment_sentences = itertools.chain(func_descr_sentences, arg_descr_sentences, return_descr_sentences)
 
         return all_comment_sentences
@@ -39,11 +40,9 @@ class Embedder:
         Get an iterator over the whole collection of the code expressions.
         :return: iterator over the language data
         """
-        return_expr_sentences = (row.split() for row in self.df['return_expr'])
-        func_name_sentences = (row.split() for row in self.df['name'] if not isinstance(row, float))
-        #TODO placeholder until arg_names are all sentences
-        self.df['arg_names'] = self.df['arg_names'].apply(lambda x: np.asarray(eval(x)))
-        arg_names_sentences = (row.split(" ") for row in self.df['arg_names'])
+        return_expr_sentences = (row.split() for row in self.return_df['return_expr_str'])
+        func_name_sentences = (row.split() for row in self.return_df['name'])
+        arg_names_sentences = (row.split() for row in self.return_df['arg_names_str'])
 
         all_comment_sentences = itertools.chain(return_expr_sentences, func_name_sentences, arg_names_sentences)
 
@@ -69,23 +68,17 @@ class Embedder:
                              #negative=20,
                              workers=cores-1)
 
-        #t = time.time()
-
         w2v_model.build_vocab(sentences=corpus_iterator,
                               progress_per=10000)
 
-        #print('Time to build vocab: {} mins'.format(round((time.time() - t) / 60, 2)))
 
         #The iterator is reset here to the beginning again
         corpus_iterator = corpus_iterator_function()
-        #t = time.time()
 
         w2v_model.train(sentences=corpus_iterator,
                         total_examples=w2v_model.corpus_count,
-                        epochs=1,
+                        epochs=20,
                         report_delay=1)
-
-        #print('Time to train the model: {} mins'.format(round((time.time() - t) / 60, 2)))
 
         w2v_model.save(model_path_name)
 
@@ -93,21 +86,38 @@ class Embedder:
         """
         Train a Word2Vec model for the descriptions and save to file.
         """
-        self.trainModel(self.getLanguageIterator, 'output/temp_language.bin')
+        self.trainModel(self.getLanguageIterator, LANGUAGE_EMBEDDING_OUTPUT_PATH)
 
     def trainCodeModel(self) -> None:
         """
         Train a Word2Vec model for the code expressions and save to file.
         """
-        self.trainModel(self.getCodeIterator, 'output/temp_code.bin')
-
-    def useModel(self):
-        wv_from_bin = Word2Vec.load("output/temp_code.bin")
-        print(wv_from_bin.wv.index2entity[:100])
-       # print(wv_from_bin.wv.most_similar(positive=["string"]))
+        self.trainModel(self.getCodeIterator, CODE_EMBEDDING_OUTPUT_PATH)
 
 
 if __name__ == '__main__':
-    embedder = Embedder("output/_temp.csv")
-    embedder.useModel()
+    param_df = pd.read_csv(PARAM_DF_PATH)
+    param_df = param_df.dropna()
+
+    return_df = pd.read_csv(RETURN_DF_PATH)
+    return_df = return_df.dropna()
+
+    if not os.path.isdir(OUTPUT_EMBEDDINGS_DIRECTORY):
+        os.mkdir(OUTPUT_EMBEDDINGS_DIRECTORY)
+
+    embedder = Embedder(param_df, return_df)
+    embedder.trainCodeModel()
+    embedder.trainLanguageModel()
+
+    w2v_language_model = Word2Vec.load(LANGUAGE_EMBEDDING_OUTPUT_PATH)
+    w2v_code_model = Word2Vec.load(CODE_EMBEDDING_OUTPUT_PATH)
+
+    print("W2V statistics: ")
+    print("W2V language model total amount of words : " + str(w2v_language_model.corpus_total_words))
+    print("W2V code model total amount of words : " + str(w2v_code_model.corpus_total_words))
+    print(" ")
+    print("Top 20 words for language model:")
+    print(w2v_language_model.wv.index2entity[:20])
+    print("\n Top 20 words for code model:")
+    print(w2v_code_model.wv.index2entity[:20])
 

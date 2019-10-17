966949607610fd2176118bc659df3339c9c4c94b
Niels <nielsdebruin@gmail.com>
2019-10-17 19:20:37 +0200

    Full learning stage rewrite

--- learn.py
+++ learn.py
@@ -1,57 +1,80 @@
-# Based on:
-# https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/bidirectional_recurrent_neural_network/main.py
+import os
+import pickle
+import time
 
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils import data
-import torchvision
-import torchvision.transforms as transforms
-import numpy as np
-import torch.nn.functional as F
 from sklearn.metrics import classification_report
+from torch.utils import data
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
 
+MODEL_DIR = "./output/models/"
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-# Bidirectional recurrent neural network (many-to-one)
+def count_model_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def store_model(model, filename, model_dir=MODEL_DIR):
+    os.makedirs(model_dir, exist_ok=True)
+    with open(os.path.join(model_dir, filename), 'wb') as f:
+        pickle.dump(model, f)
+
+
+def load_model(filename, model_dir=MODEL_DIR):
+    with open(os.path.join(model_dir, filename), 'rb') as f:
+        return pickle.load(f)
+
+
 class BiRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers):
         super(BiRNN, self).__init__()
+
         self.hidden_size = hidden_size
+
         self.num_layers = num_layers
+
+        self.dropout = nn.Dropout(p=0.2)
+
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers,
-                            batch_first=True, bidirectional=True, dropout=0.2)
-        self.lin = nn.Linear(256, 1000)
+                            batch_first=True, bidirectional=True)
+
+        self.linear = nn.Linear(hidden_size*2, 1000)
 
     def forward(self, x):
-        # Set initial states
-        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)\
-            .to(device)  # 2 for bidirection
-        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)\
-            .to(device)
+        x = self.dropout(x)
 
         # Forward propagate LSTM
-        # out: tensor of shape (batch_size, seq_length, hidden_size*2)
-        out, _ = self.lstm(x, (h0, c0))
+        # Out: tensor of shape (batch_size, seq_length, hidden_size*2)
+        x, _ = self.lstm(x)
 
         # Decode the hidden state of the last time step
-        out = out[:, -1, :]
-        out = self.lin(out)
-        return out
+        x = x[:, -1, :]
+
+        # Output layer
+        x = self.linear(x)
+
+        return x
 
 
 def load_dataset(batch_size):
-    X = np.concatenate((
-        np.load("./resources/param_datapoints_x.npy"),
-        np.load("./resources/return_datapoints_x.npy")
-    ))
-
-    y = np.concatenate((
-        np.argmax(np.load("./resources/param_datapoints_y.npy"), axis=1),
-        np.argmax(np.load("./resources/return_datapoints_y.npy"), axis=1)
-    ))
+    # X = np.concatenate((
+    #     # np.load("./output/vectors/param_datapoints_x.npy"),
+    #     np.load("./output/vectors/return_datapoints_x.npy")
+    # ))
+    #
+    # y = np.concatenate((
+    #     # np.argmax(np.load("./output/vectors/param_datapoints_y.npy"), axis=1),
+    #     np.argmax(np.load("./output/vectors/return_datapoints_y.npy"), axis=1)
+    # ))
+    #
+    X = np.load("./output/vectors/return_datapoints_x.npy")[0:10000]
+    y = np.argmax(np.load("./output/vectors/return_datapoints_y.npy"), axis=1)[0:10000]
 
     X = torch.from_numpy(X).float()
     y = torch.from_numpy(y).long()
@@ -69,59 +92,97 @@ def load_dataset(batch_size):
     return train_loader, test_loader
 
 
-def learn():
+def make_batch_prediction(model, X):
+    model.eval()
+    with torch.no_grad():
+        # Compute model output
+        outputs = model(X)
+        # Max for each label
+        labels = torch.argmax(F.softmax(outputs), 1)
+        return outputs, labels
 
-    sequence_length = 55
-    input_size = 100  # The number of expected features in the input `x`
-    hidden_size = 128  # 128x2 = 256
-    num_layers = 1
-    batch_size = 256
-    num_epochs = 12
-    learning_rate = 0.002
 
-    # Load data
-    train_loader, test_loader = load_dataset(batch_size)
+def evaluate(model: nn.Module, data_loader: DataLoader):
+    batch_count = len(data_loader)
+    true_labels = []
+    predicted_labels = []
 
-    # Load model
-    model = BiRNN(input_size, hidden_size, num_layers).to(device)
+    for i, (batch, labels) in enumerate(data_loader):
+        _, batch_labels = make_batch_prediction(model, batch.to(device))
+        predicted_labels.append(batch_labels)
+        true_labels.append(labels)
+
+    true_labels = np.hstack(true_labels)
+    predicted_labels = np.hstack(predicted_labels)
+
+    return true_labels, predicted_labels
+
+
+def train_loop(model: nn.Module, data_loader: DataLoader, model_config: dict, model_store_dir, save_each_x_epochs=1):
+    model.train()
 
     # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()  # CrossEntropyLoss?
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=model_config['learning_rate'])
 
     # Train the model
-    total_step = len(train_loader)
-    for epoch in range(num_epochs):
-        for i, (datapoints, labels) in enumerate(train_loader):
-            datapoints = datapoints.to(device)
-            labels = labels.to(device)
+    total_step = len(data_loader)
 
+    for epoch in range(1, model_config['num_epochs'] + 1):
+        for batch_i, (batch, labels) in enumerate(data_loader):
+            batch = batch.to(device)
+            labels = labels.to(device)
+            F.cross_entropy()
             # Forward pass
-            outputs = model(datapoints)
+            optimizer.zero_grad()
+            outputs = model(batch)
             loss = criterion(outputs, labels)
 
             # Backward and optimize
-            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            if (i+1) % 100 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss:{:.10f}'
-                      .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            if batch_i % 100 == 0:
+                print(f'Epoch [{epoch}/{model_config["num_epochs"]}], Batch: [{batch_i}/{total_step}], '
+                      f'Loss:{loss.item():.10f}')
+                if device == 'cuda':
+                    print(f"Cuda v-memory allocated {torch.cuda.memory_allocated()}")
 
-    # Test the model
-    # TODO: this is temporary
-    with torch.no_grad():
-        predicted = []
-        actual = []
-        for datapoints, labels in test_loader:
-            datapoints = datapoints.to(device)
-            outputs = model(datapoints)
-            predicted.append(np.argmax(outputs.data.cpu().numpy(), axis=1))
-            actual.append(labels.data.cpu().numpy())
-        predicted = np.hstack(np.array(predicted))
-        actual = np.hstack(np.array(actual))
-        print(classification_report(actual, predicted))
+        if epoch % save_each_x_epochs == 0:
+            print("Storing model!")
+            store_model(model, f"model_{model.__class__.__name__}_e_{epoch}_l_{loss.item():0.10f}.h5",
+                        model_dir=os.path.join(MODEL_DIR, model_store_dir))
 
 
-learn()
+if __name__ == '__main__':
+    print(f"-- Using {device} for training.")
+
+    model_config = {
+        'sequence_length': 55,
+        'input_size': 100,  # The number of expected features in the input `x`
+        'hidden_size': 128,  # 128x2: 256
+        'num_layers': 1,
+        'batch_size': 32,
+        'num_epochs': 1,
+        'learning_rate': 0.002,
+    }
+
+    # Load the model
+    model = BiRNN(model_config['input_size'], model_config['hidden_size'], model_config['num_layers']).to(device)
+    print(f"-- Model Loaded: {model} with {count_model_parameters(model)} parameters.")
+
+    # Load data
+    print("-- Loading data")
+    train_loader, test_loader = load_dataset(model_config['batch_size'])
+
+    # Start training
+    # train_loop(model, train_loader, model_config, model_store_dir=str(int(time.time())))
+
+    print("-- Loading model")
+    model = load_model('1571306801/model_BiRNN_e_9_l_1.8179169893.h5')
+
+    # Evaluate model performance
+    y_true, y_pred = evaluate(model, test_loader)
+
+    # Computation of metrics...... more to come?
+    print(classification_report(y_true, y_pred))
